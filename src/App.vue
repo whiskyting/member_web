2ca1a3b768fa829dae36d36239772a1df825bc69
selena <q9002285117@gmail.com>
2023-08-29 21:09:15 +0800

    feat: login page

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+    <!-- <div class="wrapper"> -->
+      <HelloWorld/>
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
